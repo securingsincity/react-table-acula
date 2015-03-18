84d90075cf92884e297d0aacabb95fe1f89e65a6
James Hrisho <james.hrisho@gmail.com>
2015-03-17 21:33:29 -0400

    Fixes exports

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-module.exports.Pagination = require('./app/js/components/Pagination.jsx');
-module.exports.Table = require('./app/js/components/Table.jsx');
-module.exports.PageableTable = require('./app/js/components/PageableTable.jsx');
-module.exports.FilterablePageableTable = require('./app/js/components/FilterablePageableTable.jsx');
-module.exports.FilterSearch = require('./app/js/components/FilterSearch.jsx');
+module.exports.Pagination = require('./src/Pagination.jsx');
+module.exports.Table = require('./src/Table.jsx');
+module.exports.PageableTable = require('./src/PageableTable.jsx');
+module.exports.FilterablePageableTable = require('./src/FilterablePageableTable.jsx');
+module.exports.FilterSearch = require('./src/FilterSearch.jsx');
